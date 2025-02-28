0fbaf9ad29df0029e415fdffb3e1608238f748fb
codebymojnu <thisismojnu@gmail.com>
2025-02-28 10:39:09 +0600

    exercise 2.1 solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,104 @@
-// Add Event Handelar in JSX
+import { useState } from "react";
 
-function AlertButton({ message, children }) {
+function UsersTable({ users, handleUserInfo, handleUserDelete }) {
   return (
-    <button
-      onClick={() => alert(message)}
-      className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-all cursor-pointer"
-    >
-      {children}
-    </button>
+    <div className="overflow-x-auto shadow-lg rounded-lg">
+      <table className="min-w-full table-auto">
+        <thead className="bg-gray-100">
+          <tr>
+            <th className="px-4 py-2 text-left font-medium text-gray-700">
+              Name
+            </th>
+            <th className="px-4 py-2 text-left font-medium text-gray-700">
+              Mobile
+            </th>
+            <th className="px-4 py-2 text-left font-medium text-gray-700">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr
+              key={user.id}
+              className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
+              onClick={() => handleUserInfo(user?.id)}
+            >
+              <td className="px-4 py-2 text-gray-800">{user?.name}</td>
+              <td className="px-4 py-2 text-gray-800">{user?.email}</td>
+              <td
+                className="px-4 py-2 text-red-500 cursor-pointer hover:text-red-700"
+                onClick={(e) => handleUserDelete(e, user?.id)}
+              >
+                Delete
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function UserProfile({ user }) {
+  return (
+    <div>
+      <p>Name: {user?.name}</p>
+      <p>User ID: {user?.id}</p>
+    </div>
   );
 }
 
 export default function App() {
-  // add event handler to button JSX
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+    },
+    {
+      id: 3,
+      name: "Mark Johnson",
+      email: "mark.johnson@example.com",
+    },
+    {
+      id: 4,
+      name: "Emily Davis",
+      email: "emily.davis@example.com",
+    },
+    {
+      id: 5,
+      name: "Michael Brown",
+      email: "michael.brown@example.com",
+    },
+  ]);
+
+  const [user, setUser] = useState(null);
+
+  function handleUserInfo(id) {
+    const userInfo = users?.filter((user) => user?.id == id);
+    setUser(userInfo[0]);
+  }
+
+  function handleUserDelete(e, id) {
+    e.stopPropagation();
+    const presentUsers = users?.filter((user) => user?.id !== id);
+    setUsers(presentUsers);
+  }
+
   return (
-    <div className="flex justify-center mt-4 gap-3">
-      <AlertButton message="Playing Movie...">Play this Movie</AlertButton>
-      <AlertButton message="Image Uploading...">Upload Image</AlertButton>
-    </div>
+    <>
+      {user && <UserProfile user={user} />}
+      <UsersTable
+        users={users}
+        handleUserInfo={handleUserInfo}
+        handleUserDelete={handleUserDelete}
+      />
+    </>
   );
 }
---
+++ src/users.js
@@ -0,0 +1,27 @@
+export const users = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+    },
+    {
+        id: 2,
+        name: 'Jane Smith',
+        email: 'jane.smith@example.com'
+    },
+    {
+        id: 3,
+        name: 'Mark Johnson',
+        email: 'mark.johnson@example.com'
+    },
+    {
+        id: 4,
+        name: 'Emily Davis',
+        email: 'emily.davis@example.com'
+    },
+    {
+        id: 5,
+        name: 'Michael Brown',
+        email: 'michael.brown@example.com'
+    }
+];
